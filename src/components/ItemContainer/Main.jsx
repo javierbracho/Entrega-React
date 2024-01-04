8b9883c84b3b671b53738ef435544722d9f58f6e
javierbracho <Javier@MacBook-Pro-de-Victoria.local>
2024-01-04 20:10:15 -0300

    se modifica rutas de main

--- src/components/ItemContainer/Main.jsx
+++ src/components/ItemContainer/Main.jsx
@@ -5,11 +5,11 @@ const Main = () => {
   return (
     <div className='contenedor-principal'>  
       <div className='contenedor-secundario'>
-        <img src="../../../public/img/conlogo.png" alt="" />
+        <img src="/img/conlogo.png" alt="" />
       </div>
       <div className='contenedor-secundario-imgS'>
-        <img src="../../../public/img/mainnuevo.png" alt="" />
-        <img src="../../../public/img/main22.png" alt="" />
+        <img src="/img/mainnuevo.png" alt="" />
+        <img src="/img/main22.png" alt="" />
       </div>
 
     </div>
